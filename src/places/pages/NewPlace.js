1808e58701842efd7877ac520b664605f93894cc
PERDOMO Juan Valentin <juanvalentin.perdomo@amaris.com>
2023-01-09 18:51:37 -0500

    add input validators

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,8 +1,31 @@
-import React from 'react'
-import './NewPlaces.css'
-import Input from './../../shared/components/FormElements/Input';
+import React, { useCallback } from "react";
+import "./NewPlaces.css";
+import Input from "./../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "./../../util/validators";
 export const NewPlace = () => {
-  return <form className="place-form">
-    <Input element="input" type="text" label="New Place"/>
-  </form>
-}
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  return (
+    <form className="place-form">
+      <Input
+      id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={titleInputHandler}
+        errorText="please enter a valid title here"
+      />
+      <Input
+      id="description"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE(),VALIDATOR_MINLENGTH(5)]}
+        onInput={descriptionInputHandler}
+        errorText="please enter a valid description (at least 5 characters). "
+      />
+    </form>
+  );
+};
